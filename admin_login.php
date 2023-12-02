0ed095d0c8b3619b1e91fdef83bb93d148a68cb7
justinejay01 <66052057+justinejay01@users.noreply.github.com>
2023-12-02 14:35:09 +0800

    update files

--- admin_login.php
+++ admin_login.php
@@ -1,20 +1,54 @@
 <?php
- include "conn.php";
+require "conn.php";
 
- $id = $_POST["id"];
- $username = $_POST["username"];
- $password = $_POST["password"];
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
 
- $sql = "SELECT id, username, password FROM b_admin WHERE password = '" .$password. "' AND (id = '" .$id. "' OR username = '" .$username. "');";
-$result = $conn->query($sql);
+  $errors = array();
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+  if ($stmt = $conn->prepare("SELECT username, password FROM admin_acc WHERE username = ? LIMIT 1")) {
+
+    /* bind parameters for markers */
+    $stmt->bind_param('s', $username);
+
+    /* execute query */
+    if ($stmt->execute()) {
+
+      /* store result */
+      $stmt->store_result();
+
+      if ($stmt->num_rows > 0) {
+        /* bind result variables */
+        $stmt->bind_result($username_tmp, $password_hash);
+
+        /* fetch value */
+        $stmt->fetch();
+
+        if (password_verify($password, $password_hash)) {
+          echo $username_tmp;
+
+          return;
+        } else {
+          $errors[] = "Wrong email or password.";
+        }
+      } else {
+        $errors[] = "Wrong email or password.";
+      }
+
+      /* close statement */
+      $stmt->close();
+    } else {
+      $errors[] = "Something went wrong, please try again.";
+    }
+  } else {
+    $errors[] = "Something went wrong, please try again.";
+  }
+
+  if (count($errors) > 0) {
+    echo $errors[0];
   }
 } else {
-  echo "0 results";
+  echo "Missing data";
 }
-$conn->close();
 ?>
--- admin_register.php
+++ admin_register.php
@@ -1,23 +1,132 @@
 <?php
- include "conn.php";
+require "conn.php";
 
- $id = $_POST["id"];
- $username = $_POST["username"];
- $password = $_POST["password"];
- $firstname = $_POST["firstname"];
- $lastname = $_POST["lastname"];
- $email = $_POST["email"];
- $department = $_POST["department"];
+if (isset($_POST["id"]) && isset($_POST["username"]) && isset($_POST["password"]) && isset($_POST["firstname"]) && isset($_POST["lastname"]) && isset($_POST["email"]) && isset($_POST["department"])) {
+  $id = $_POST["id"];
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+  $firstname = $_POST["firstname"];
+  $lastname = $_POST["lastname"];
+  $email = $_POST["email"];
+  $department = $_POST["department"];
 
- $sql = "INSERT INTO b_admin (id, username, password, firstname, lastname, email, department)
- VALUES ('" .$id."', '" .$username. "', '" .$password. "', '" .$firstname. "', '" .$lastname. "', '" .$email. "', '" .$department. "')";
- 
- if ($conn->query($sql) === TRUE) {
-   echo "1";
- } else {
-   echo "Error: " . $sql . "<br>" . $conn->error;
- }
- 
- $conn->close();
- 
+  $emailMaxLength = 254;
+  $usernameMaxLength = 20;
+  $usernameMinLength = 3;
+  $passwordMaxLength = 19;
+  $passwordMinLength = 5;
+  $errors = array();
+
+  //Validate email
+  if (preg_match('/\s/', $email)) {
+    $errors[] = "Email can't have spaces";
+  } else {
+    if (!validate_email_address($email)) {
+      $errors[] = "Invalid email";
+    } else {
+      if (strlen($email) > $emailMaxLength) {
+        $errors[] = "Email is too long, must be equal or under " . strval($emailMaxLength) . " characters";
+      }
+    }
+  }
+
+  //Validate username
+  if (strlen($username) > $usernameMaxLength || strlen($username) < $usernameMinLength) {
+    $errors[] = "Incorrect username length, must be between " . strval($usernameMinLength) . " and " . strval($usernameMaxLength) . " characters";
+  } else {
+    if (!ctype_alnum($username)) {
+      $errors[] = "Username must be alphanumeric";
+    }
+  }
+
+  //Validate password
+  if (preg_match('/\s/', $password)) {
+    $errors[] = "Password can't have spaces";
+  } else {
+    if (strlen($password) > $passwordMaxLength || strlen($password) < $passwordMinLength) {
+      $errors[] = "Incorrect password length, must be between " . strval($passwordMinLength) . " and " . strval($passwordMaxLength) . " characters";
+    } else {
+      if (!preg_match('/[A-Za-z]/', $password) || !preg_match('/[0-9]/', $password)) {
+        $errors[] = "Password must contain atleast 1 letter and 1 number";
+      }
+    }
+  }
+
+  //Check if there is user already registered with the same email or username
+  if (count($errors) == 0) {
+    //Connect to database
+
+    if ($stmt = $conn->prepare("SELECT username, email FROM admin_acc WHERE email = ? OR username = ? LIMIT 1")) {
+
+      /* bind parameters for markers */
+      $stmt->bind_param('ss', $email, $username);
+
+      /* execute query */
+      if ($stmt->execute()) {
+
+        /* store result */
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+
+          /* bind result variables */
+          $stmt->bind_result($username_tmp, $email_tmp);
+
+          /* fetch value */
+          $stmt->fetch();
+
+          if ($email_tmp == $email) {
+            $errors[] = "User with this email already exist.";
+          } else if ($username_tmp == $username) {
+            $errors[] = "User with this name already exist.";
+          }
+        }
+
+        /* close statement */
+        $stmt->close();
+      } else {
+        $errors[] = "Something went wrong, please try again.";
+      }
+    } else {
+      $errors[] = "Something went wrong, please try again.";
+    }
+  }
+
+  //Finalize registration
+  if (count($errors) == 0) {
+    $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+    if ($stmt = $conn->prepare("INSERT INTO admin_acc (id, firstname, lastname, department, email, username, password) VALUES(?, ?, ?, ?, ?, ?, ?)")) {
+
+      /* bind parameters for markers */
+      $stmt->bind_param('sssssss', $id, $firstname, $lastname, $department, $email, $username, $hashedPassword);
+
+      /* execute query */
+      if ($stmt->execute()) {
+
+        /* close statement */
+        $stmt->close();
+      } else {
+        $errors[] = "Something went wrong, please try again.";
+      }
+    } else {
+      $errors[] = "Something went wrong, please try again.";
+    }
+  }
+
+  if (count($errors) > 0) {
+    echo $errors[0];
+  } else {
+    echo $username;
+  }
+
+  $conn->close();
+} else {
+  echo "Missing Data";
+}
+
+function validate_email_address($email)
+{
+  return preg_match('/^([a-z0-9!#$%&\'*+-\/=?^_`{|}~.]+@[a-z0-9.-]+\.[a-z0-9]+)$/i', $email);
+}
+
 ?>
